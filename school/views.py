b016a96e6be3e28a7f4a956a08fa15de6ae42d71
Ksenia Lozovik <ksy_rinn@mail.ru>
2023-06-20 00:21:20 +0300

    add readme

---
+++ school/views.py
@@ -0,0 +1,17 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+class PublisherListView(ListView):
+    model = Student
+def students_list(request):
+    object_list = Student.objects.all()
+    template = 'school/students_list.html'
+    context = {'students': object_list}
+    print(context)
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    ordering = 'group'
+
+    return render(request, template, context=context)
